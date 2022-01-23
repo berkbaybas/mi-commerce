d566a09a4764f2583c7cc5162fd3af6c0333e610
berk <berk_baybas@hotmail.com>
2022-01-23 21:08:48 +0300

    react router scroll-to-top when changing path

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Header from './components/Header'
 import ProductList from './pages/Product/ProductList'
 import ProductDetail from './pages/Product/ProductDetail'
 import Cart from './pages/Cart'
+import ScrollToTop from './components/shared/ScrollToTop'
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Header />
+        <ScrollToTop />
         <Routes>
           <Route exact path="/" element={<ProductList />}></Route>
           <Route path="/product/:id" element={<ProductDetail />}></Route>
